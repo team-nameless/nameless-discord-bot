ac55f1e3c4e000dc23e4f758d7f2238ed9ad1b65
Tien Dat Pham <dat20036@gmail.com>
2022-04-11 20:34:29 +0700

    Stuffs

--- main.py
+++ main.py
@@ -34,6 +34,5 @@ async def on_error(event_name: str, *args, **kwargs):
 # client.add_cog(cogs.slash.TestSlashCog(client))
 client.add_cog(cogs.slash.OwnerSlashCog(client))
 client.add_cog(cogs.slash.MusicSlashCog(client))
-client.add_cog(cogs.slash.ActivitySlashCog(client))
 
 client.run(config.TOKEN)

d1f5f8c626c905776c1cd9219d64b95731897451
Tien Dat Pham <dat20036@gmail.com>
2022-04-14 08:41:09 +0700

    Add default values

--- database/models.py
+++ database/models.py
@@ -14,18 +14,18 @@ Base = declarative_base()
 class DbUser(Base):
     __tablename__ = "Users"
     id: int = Column(BigInteger, name="Id", primary_key=True)
-    warn_count: int = Column(SmallInteger, name="WarnCount")
-    osu_username: str = Column(Text, name="OsuUsername")
-    osu_mode: str = Column(Text, name="OsuMode")
+    warn_count: int = Column(SmallInteger, name="WarnCount", default=0)
+    osu_username: str = Column(Text, name="OsuUsername", default="")
+    osu_mode: str = Column(Text, name="OsuMode", default="")
 
 
 class DbGuild(Base):
     __tablename__ = "Guilds"
     id: int = Column(BigInteger, name="Id", primary_key=True)
-    is_welcome_enabled: bool = Column(Boolean, name="IsWelcomeEnabled")
-    is_goodbye_enabled: bool = Column(Boolean, name="IsGoodbyeEnabled")
-    welcome_channel_id: int = Column(BigInteger, name="WelcomeChannelId")
-    goodbye_channel_id: int = Column(BigInteger, name="GoodbyeChannelId")
-    welcome_message: str = Column(UnicodeText, name="WelcomeMessage")
-    goodbye_message: str = Column(UnicodeText, name="GoodbyeMessage")
-    radio_start_time: datetime.datetime = Column(DateTime, name="RadioStartTime")
+    is_welcome_enabled: bool = Column(Boolean, name="IsWelcomeEnabled", default=False)
+    is_goodbye_enabled: bool = Column(Boolean, name="IsGoodbyeEnabled", default=False)
+    welcome_channel_id: int = Column(BigInteger, name="WelcomeChannelId", default=0)
+    goodbye_channel_id: int = Column(BigInteger, name="GoodbyeChannelId", default=0)
+    welcome_message: str = Column(UnicodeText, name="WelcomeMessage", default="")
+    goodbye_message: str = Column(UnicodeText, name="GoodbyeMessage", default="")
+    radio_start_time: datetime.datetime = Column(DateTime, name="RadioStartTime", default=datetime.datetime.min)

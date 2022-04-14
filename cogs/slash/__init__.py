aad3b38e2e0503a19f46b33d7db48c4182f4b926
Tien Dat Pham <dat20036@gmail.com>
2022-04-14 19:00:47 +0700

    It was a long journey, man.

--- cogs/slash/__init__.py
+++ cogs/slash/__init__.py
@@ -1,5 +1,4 @@
 from .OwnerSlashCog import *
-from .TestSlashCog import *
 from .MusicSlashCog import *
 from .ActivitySlashCog import *
 from .ModeratorSlashCog import *
--- main.py
+++ main.py
@@ -49,8 +49,6 @@ async def close():
     log_sql_pool.warning(msg="Shutting down pool")
     globals.CRUD.close_all_sessions()
 
-# Please DO NOT add TestSlashCog in code, even if your mom told you so.
-# client.add_cog(cogs.slash.TestSlashCog(client))
 client.add_cog(cogs.slash.OwnerSlashCog(client))
 client.add_cog(cogs.slash.MusicSlashCog(client))
 client.add_cog(cogs.slash.ActivitySlashCog(client))

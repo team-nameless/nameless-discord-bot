b861ee7b0d69a09629d4106834fa5a59f6452e6a
Tien Dat Pham <dat20036@gmail.com>
2022-04-16 12:04:13 +0700

    Add remaining pieces of osu! commands code

--- cogs/slash/OsuSlashCog.py
+++ cogs/slash/OsuSlashCog.py
@@ -1,11 +1,11 @@
 import datetime
 from typing import Optional, List
 
-from nextcord.ext.menus import Menu, PageFormatType
+from nextcord_paginator import Paginator
 from ossapi import *
 import nextcord
 from nextcord import Color, SlashOption
-from nextcord.ext import commands, menus
+from nextcord.ext import commands
 
 from config import Config
 import globals
@@ -38,14 +38,6 @@ def convert_to_game_mode(mode: str) -> GameMode:
             return GameMode.CTB
         case "mania":
             return GameMode.MANIA
-
-
-class OsuScoresPageSource(menus.ListPageSource):
-    def __init__(self, data):
-        super().__init__(data, per_page=1)
-
-    async def format_page(self, menu: Menu, entries: List[Score]) -> PageFormatType:
-        embed = nextcord.Embed(title)
 
 
 class FailInclusionConfirmationView(nextcord.ui.View):
@@ -109,6 +101,8 @@ class OsuSlashCog(commands.Cog):
 
     async def __generic_check(self, interaction: nextcord.Interaction, request: str, username: str, mode: str,
                               is_from_context: bool = False):
+        await interaction.edit_original_message(content="Processing")
+
         osu_user: Optional[User]
         the_mode = None
 
@@ -147,9 +141,8 @@ class OsuSlashCog(commands.Cog):
                                                        f"{user_stats.grade_counts.s}/"
                                                        f"{user_stats.grade_counts.a}")
 
-            await interaction.edit_original_message(embeds=[eb])
+            await interaction.edit_original_message(content="", embed=eb)
         else:
-            """
             request_type: ScoreType = ScoreType.BEST
 
             if request == "bests":
@@ -160,37 +153,93 @@ class OsuSlashCog(commands.Cog):
                 request_type = ScoreType.RECENT
 
             limit: int
-            include_fails: bool = True
+            prompt: str
+            include_fails: bool
+            msg: nextcord.Message
+
+            # limit count
+            if not is_from_context:
+                try:
+                    await interaction.send(content="How many records do you want to get?")
+                    msg = await self.bot.wait_for("message",
+                                                  check=Utility.message_waiter(interaction),
+                                                  timeout=30)
+                    prompt = msg.content
+                    await msg.delete(delay=1.0)
+                except TimeoutError:
+                    await interaction.edit_original_message(content="Timed out")
+                    return
+            else:
+                prompt = "1"
 
             try:
-                msg: nextcord.Message = self.bot.wait_for("message",
-                                                          check=Utility.message_waiter(interaction),
-                                                          timeout=30)
+                limit = int(prompt)
+            except ValueError:
+                await interaction.followup.send(content="Invalid number provided. Please correct then run again.")
+                return
 
-                try:
-                    limit = int(msg.content)
+            # fail inclusion prompt
+            if not is_from_context and request == "recents":
+                view = FailInclusionConfirmationView()
+                await interaction.followup.send(content="Do you want to include fail scores?", view=view)
+                await view.wait()
 
-                    if not is_from_context:
-                        view = FailInclusionConfirmationView()
-                        await interaction.edit_original_message(content="Do you want to include fail scores?",
-                                                                view=view)
-                        await view.wait()
+                if not view.is_confirmed:
+                    await interaction.followup.send(content="Timed out")
+                    return
+                else:
+                    include_fails = view.is_confirmed
+            else:
+                include_fails = True
 
-                        if not view.is_confirmed:
-                            await interaction.followup.send(content="Timed out")
-                        else:
-                            include_fails = view.is_confirmed
+            scores: List[Score] = self.api.user_scores(osu_user.id, request_type, include_fails, the_mode, limit)
 
-                    scores = self.api.user_scores(osu_user.id, request_type, include_fails, the_mode, limit)
+            if len(scores) == 0:
+                await interaction.followup.send(content="No suitable scores found")
+                return
 
-                except ValueError:
-                    await interaction.followup.send(content="Invalid number provided. Please run the command again and"
-                                                            "correct the input.")
-            except TimeoutError:
-                await interaction.followup.send(content="Timed out")
-            """
-            await interaction.edit_original_message(content="Not implemented")
-            return
+            embeds = []
+
+            for idx, score in enumerate(scores):
+                beatmap_set = score.beatmapset
+                beatmap = score.beatmap
+                sender = score.user()
+                score_stats = score.statistics
+
+                embed = nextcord.Embed(
+                    description=f"Score position #{idx + 1}",
+                    color=Color.brand_red(),
+                    timestamp=datetime.datetime.now()
+                ) \
+                    .set_author(name=f"{beatmap_set.artist} - {beatmap_set.title} [{beatmap.version}] "
+                                     f"+{score.mods.long_name().replace(' ', '')}",
+                                url=beatmap.url,
+                                icon_url=sender.avatar_url) \
+                    .set_thumbnail(url=beatmap_set.covers.cover_2x) \
+                    .add_field(name="Score",
+                               value=f"{sender.country_code} #{score.rank_country} - GLB #{score.rank_global}",
+                               inline=False) \
+                    .add_field(name="Ranking", value=score.rank.name) \
+                    .add_field(name="Accuracy", value=score.accuracy) \
+                    .add_field(name="Max combo", value=f"{score.max_combo}x/{beatmap.max_combo}") \
+                    .add_field(name="Hit count", value=f"{score_stats.count_300}/"
+                                                       f"{score_stats.count_100}/"
+                                                       f"{score_stats.count_50}/"
+                                                       f"{score_stats.count_miss}") \
+                    .add_field(name="PP", value=f"{score.pp} * {score.weight.percentage}% = {score.weight.pp}"
+                               if score.weight is not None else "0") \
+                    .add_field(name="Submission time", value=score.created_at)
+
+                embeds.append(embed)
+
+            if not is_from_context or len(embeds) != 1:
+                msg = await interaction.followup.send(embed=embeds[0])
+                pages = Paginator(msg, embeds, interaction.user, interaction.client, timeout=60, footerpage=False,
+                                  footerdatetime=False,
+                                  footerboticon=False)
+                await pages.start()
+            else:
+                await interaction.followup.send(embed=embeds[0])
 
     @osu.subcommand(description="Check osu! profile of a member")
     async def check_member(self, interaction: nextcord.Interaction,

fb416a01897bd13f4e8d2328ff8e8f4e573ab18e
Tien Dat Pham <dat20036@gmail.com>
2022-04-16 12:15:07 +0700

    kaboom

--- Lilia/Database/Models/DbUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lilia.Database.Models;
-
-public class DbUser
-{
-	public ulong Id { get; set; }
-	public short WarnCount { get; set; }
-	public string OsuMode { get; set; }
-	public string OsuUsername { get; set; }
-}
--- Lilia/Modules/Utils/GuildConfigModuleUtils.cs
+++
@@ -1,8 +0,0 @@
-﻿using Discord.Interactions;
-
-namespace Lilia.Modules.Utils;
-
-public static class GuildConfigModuleUtils
-{
-	public static bool IsChannelExist(ShardedInteractionContext ctx, ulong testId) => ctx.Guild.GetChannel(testId) != null;
-}

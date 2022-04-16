e2fb2700a71cbf9ffc0d3bf28433cc5ec56e5389
Tien Dat Pham <dat20036@gmail.com>
2022-04-16 19:26:52 +0700

    Use official extension for activity creation.
    And add missing stuffs.

--- cogs/slash/ActivitySlashCog.py
+++ cogs/slash/ActivitySlashCog.py
@@ -1,8 +1,6 @@
 import nextcord
 from nextcord import SlashOption
-from nextcord.ext import commands
-
-from discord_together import DiscordTogether, discordTogetherMain
+from nextcord.ext import commands, activities
 
 from config import Config
 
@@ -18,15 +16,16 @@ class ActivitySlashCog(commands.Cog):
     @activity.subcommand(description="Create a embedded activity")
     async def create(self,
                      interaction: nextcord.Interaction,
-                     activity: str = SlashOption(description="Your desired activity",
-                                                 choices=discordTogetherMain.defaultApplications)):
+                     target: str = SlashOption(description="Your desired activity",
+                                                 choices={
+                                                     str(i.name): str(i.value) for i in activities.enums.Activity
+                                                 }),
+                     voice_channel: nextcord.abc.GuildChannel = SlashOption(description="Target voice channel",
+                                                                        channel_types=[nextcord.ChannelType.voice])):
         await interaction.response.send_message("Generating link")
 
-        if not interaction.user.voice:
-            await interaction.edit_original_message(content="You need to be in a voice channel")
-            return
+        # voice_channel: nextcord.VoiceChannel
+        # voice_channel.create_activity_invite = activities.create_activity_invite
+        inv = await voice_channel.create_activity_invite(activities.Activity.custom, activity_id=int(target))
 
-        together: DiscordTogether = await DiscordTogether(Config.TOKEN)
-        link: str = await together.create_link(interaction.user.voice.channel.id, activity)
-
-        await interaction.edit_original_message(content=f"Here is your link: {link}")
+        await interaction.edit_original_message(content=f"Here is your link: {inv.url}")

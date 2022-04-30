7ebc5136091201bc2bd0071782e86ffcb021bd6e
Tien Dat Pham <dat20036@gmail.com>
2022-04-30 21:14:44 +0700

    Upload stable code of MongoDB

--- database/crud.py
+++ database/crud.py
@@ -1,10 +1,13 @@
 from typing import Optional
 
 import nextcord
+from pymongo.collection import Collection
+from pymongo.database import Database
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import close_all_sessions
 from sqlalchemy.util import IdentitySet
+import pymongo
 
 from config import Config
 from customs import Utility
@@ -13,42 +16,62 @@ from .models import Base, DbUser, DbGuild
 
 class CRUD:
     """
-    Basic PostgreSQL CRUD operations required to make this project work.
+    Basic database CRUD operations.
     """
 
+    def __should_mongo(self) -> bool:
+        if Config.DATABASE and Config.MONGODB:
+            self.is_mongo = True
+        elif not Config.DATABASE and Config.MONGODB:
+            self.is_mongo = True
+        elif Config.DATABASE and not Config.MONGODB:
+            self.is_mongo = False
+        else:
+            self.is_mongo = False
+
+        return self.is_mongo
+
     def __init__(self):
-        self.engine = create_engine(
-            Utility.get_db_url(), logging_name=Config.DATABASE["db_name"]
-        )
-        _session = sessionmaker(bind=self.engine)
-        self.session = _session()
+        if self.__should_mongo():
+            # I know, the db_name is used twice, can't fix that
+            self.mongo_engine: Database = pymongo.MongoClient(
+                Utility.get_mongo_db_url()
+            )[Config.MONGODB["db_name"]]
+
+            self.mongo_guilds: Collection = self.mongo_engine.get_collection(
+                DbGuild.__tablename__
+            )
+            self.mongo_users: Collection = self.mongo_engine.get_collection(
+                DbUser.__tablename__
+            )
+        else:
+            self.engine = create_engine(
+                Utility.get_db_url(), logging_name=Config.DATABASE["db_name"]
+            )
+            _session = sessionmaker(bind=self.engine)
+            self.session = _session()
+            Base.metadata.create_all(self.engine)
 
     @property
     def current_session(self) -> sessionmaker():
         """
-        Current session
+        Current session, None in MongoDB.
         """
         return self.session
 
     @property
     def dirty(self) -> IdentitySet:
         """
-        The data that is modified, but not updated to database.
+        The data that is modified, but not updated to database, None in MongoDB.
         """
         return self.session.dirty
 
     @property
     def new(self) -> IdentitySet:
         """
-        The PENDING new data.
+        The PENDING new data, None in MongoDB.
         """
         return self.session.new
-
-    def init(self) -> None:
-        """
-        Normally you don't need to care about this.
-        """
-        Base.metadata.create_all(self.engine)
 
     def get_or_create_user_record(self, user: nextcord.User) -> tuple[DbUser, bool]:
         """
@@ -75,60 +98,107 @@ class CRUD:
             return g, False
 
     def __get_user_record(self, user: nextcord.User) -> Optional[DbUser]:
+        if self.is_mongo:
+            record = self.mongo_users.find_one({"id": user.id})
+            if record:
+                return DbUser.from_dict(dict(record))
+            else:
+                return None
+
         return self.session.query(DbUser).filter_by(id=user.id).one_or_none()
 
     def __get_guild_record(self, guild: nextcord.Guild) -> Optional[DbGuild]:
+        if self.is_mongo:
+            record = self.mongo_guilds.find_one({"id": guild.id})
+            if record:
+                return DbGuild.from_dict(dict(record))
+            else:
+                return None
+
         return self.session.query(DbGuild).filter_by(id=guild.id).one_or_none()
 
     def __create_user_record(self, user: nextcord.User) -> DbUser:
-        decoy_user = DbUser(id=user.id)
+        decoy_user = DbUser(_id=user.id)
 
-        if not self.session.query(DbUser).filter_by(id=user.id).one_or_none():
-            self.session.add(decoy_user)
-            self.save_changes()
+        if self.is_mongo:
+            self.mongo_users.insert_one(decoy_user.to_dict())
             return decoy_user
         else:
-            return self.session.query(DbUser).filter_by(id=user.id).one()
+            if not self.session.query(DbUser).filter_by(id=user.id).one_or_none():
+                self.session.add(decoy_user)
+                self.save_changes()
+                return decoy_user
+            else:
+                return self.session.query(DbUser).filter_by(id=user.id).one()
 
     def __create_guild_record(self, guild: nextcord.Guild) -> DbGuild:
-        decoy_guild = DbGuild(id=guild.id)
+        decoy_guild = DbGuild(_id=guild.id)
 
-        if not self.session.query(DbGuild).filter_by(id=guild.id).one_or_none():
-            self.session.add(decoy_guild)
-            self.save_changes()
+        if self.is_mongo:
+            self.mongo_guilds.insert_one(decoy_guild.to_dict())
             return decoy_guild
         else:
-            return self.session.query(DbGuild).filter_by(id=guild.id).one()
+            if not self.session.query(DbGuild).filter_by(id=guild.id).one_or_none():
+                self.session.add(decoy_guild)
+                self.save_changes()
+                return decoy_guild
+            else:
+                return self.session.query(DbGuild).filter_by(id=guild.id).one()
 
     def delete_guild_record(self, guild_record: DbGuild) -> None:
         """
         Delete a guild record from the database.
         :param guild_record: Guild record to delete.
         """
-        self.session.delete(guild_record)
+        if self.is_mongo:
+            self.mongo_guilds.delete_many({"id": guild_record.id})
+        else:
+            self.session.delete(guild_record)
 
     def delete_user_record(self, user_record: DbUser) -> None:
         """
         Delete a user record from the database.
         :param user_record: User record to delete.
         """
-        self.session.delete(user_record)
+        if self.is_mongo:
+            self.mongo_users.delete_many({"id": user_record.id})
+        else:
+            self.session.delete(user_record)
 
     def rollback(self) -> None:
         """
         Revert ALL changes made on current session. If you use this after save_changes(), congrats, you did nothing!
-        """
-        self.session.rollback()
 
-    def save_changes(self) -> None:
+        """
+        if self.is_mongo:
+            pass
+        else:
+            self.session.rollback()
+
+    def save_changes(
+        self, user_record: DbUser = None, guild_record: DbGuild = None
+    ) -> None:
         """
         Save changes made on current session. In some cases, this will clear the pending queue of rollback().
 
         "Mom, but it works in my codes!"
                     - Swyrin
         """
-        self.session.commit()
+        if self.is_mongo:
+            if user_record:
+                self.mongo_users.update_one(
+                    {"id": user_record.id}, {"$set": user_record.to_dict()}
+                )
 
-    @staticmethod
-    def close_all_sessions() -> None:
-        close_all_sessions()
+            if guild_record:
+                self.mongo_guilds.update_one(
+                    {"id": guild_record.id}, {"$set": guild_record.to_dict()}
+                )
+        else:
+            self.session.commit()
+
+    def close_all_sessions(self) -> None:
+        if self.is_mongo:
+            pass
+        else:
+            close_all_sessions()
--- database/models.py
+++ database/models.py
@@ -6,13 +6,38 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 
+class MongoABC:
+    pass
+
+
 # Migration cheat code:
 # alembic revision --autogenerate -m "Message here"
 # alembic upgrade head
 # In case of fire: https://www.learndatasci.com/tutorials/using-databases-python-postgres-sqlalchemy-and-alembic/
 
 
-class DbUser(Base):
+class DbUser(Base, MongoABC):
+    @staticmethod
+    def from_dict(d: dict):
+        return DbUser(d["id"], d["warn_count"], d["osu_mode"], d["osu_username"])
+
+    def __init__(
+        self, _id: int, warn_count: int = 0, osu_mode: str = "", osu_username: str = ""
+    ):
+        super().__init__()
+        self.id: int = _id
+        self.warn_count: int = warn_count
+        self.osu_mode: str = osu_mode
+        self.osu_username: str = osu_username
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "warn_count": self.warn_count,
+            "osu_username": self.osu_username,
+            "osu_mode": self.osu_mode,
+        }
+
     __tablename__ = "Users"
     id: int = Column(BigInteger, name="Id", primary_key=True)
     warn_count: int = Column(SmallInteger, name="WarnCount", default=0)
@@ -20,7 +45,53 @@ class DbUser(Base):
     osu_mode: str = Column(Text, name="OsuMode", default="")
 
 
-class DbGuild(Base):
+class DbGuild(Base, MongoABC):
+    def __init__(
+        self,
+        _id: int,
+        is_welcome_enabled: bool = False,
+        is_goodbye_enabled: bool = False,
+        welcome_channel_id: int = 0,
+        goodbye_channel_id: int = 0,
+        welcome_message: str = "",
+        goodbye_message: str = "",
+        radio_start_time: datetime.datetime = datetime.datetime.min,
+    ):
+        super().__init__()
+        self.id: int = _id
+        self.is_welcome_enabled: bool = is_welcome_enabled
+        self.is_goodbye_enabled: bool = is_goodbye_enabled
+        self.welcome_channel_id: int = welcome_channel_id
+        self.goodbye_channel_id: int = goodbye_channel_id
+        self.welcome_message: str = welcome_message
+        self.goodbye_message: str = goodbye_message
+        self.radio_start_time: datetime.datetime = radio_start_time
+
+    @staticmethod
+    def from_dict(d: dict):
+        return DbGuild(
+            d["id"],
+            d["is_welcome_enabled"],
+            d["is_goodbye_enabled"],
+            d["welcome_channel_id"],
+            d["goodbye_channel_id"],
+            d["welcome_message"],
+            d["welcome_message"],
+            d["radio_start_time"],
+        )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "is_welcome_enabled": self.is_welcome_enabled,
+            "is_goodbye_enabled": self.is_goodbye_enabled,
+            "welcome_channel_id": self.welcome_channel_id,
+            "goodbye_channel_id": self.goodbye_channel_id,
+            "welcome_message": self.welcome_message,
+            "goodbye_message": self.goodbye_message,
+            "radio_start_time": self.radio_start_time,
+        }
+
     __tablename__ = "Guilds"
     id: int = Column(BigInteger, name="Id", primary_key=True)
     is_welcome_enabled: bool = Column(Boolean, name="IsWelcomeEnabled", default=False)

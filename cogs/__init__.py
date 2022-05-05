b4e51fb1aa123203f138f6b058f42a6c5fa99d4f
Tien Dat Pham <dat20036@gmail.com>
2022-05-05 16:57:22 +0700

    Initial discord.py migration

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,8 +1,9 @@
 from .ActivityCog import *
-from .ConfigCog import *
-from .ExperimentalCog import *
-from .ModeratorCog import *
 
+# from .ConfigCog import *
+from .ExperimentalCog import *
+
+# from .ModeratorCog import *
 # from .MusicCog import *
-from .OsuCog import *
+# from .OsuCog import *
 from .OwnerCog import *

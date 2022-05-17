19192d3d5def8979109aaf643a055b72e4fa0b53
Tien Dat Pham <dat20036@gmail.com>
2022-05-17 17:59:46 +0700

    Add commands to load, unload and reload cog

--- cogs/OwnerCog.py
+++ cogs/OwnerCog.py
@@ -1,3 +1,5 @@
+import logging
+
 from discord import app_commands
 from discord.ext import commands
 
@@ -17,3 +19,61 @@ class OwnerCog(commands.Cog):
         """Shutdown the bot"""
         await ctx.send("Bye owo!")
         await self.bot.close()
+
+    @commands.is_owner()
+    @commands.hybrid_command()
+    @app_commands.guilds(*Config.GUILD_IDs)
+    @app_commands.describe(module_name="The Python-qualified module name")
+    async def reload(self, ctx: commands.Context, module_name: str):
+        """Reload a module"""
+        await ctx.defer()
+
+        try:
+            await self.bot.reload_extension(module_name)
+            await ctx.send(f"Done reloading {module_name}")
+        except commands.ExtensionNotFound:
+            await ctx.send(f"{module_name} was not found in the code")
+        except commands.ExtensionNotLoaded:
+            await ctx.send(f"{module_name} was not loaded before")
+
+    @commands.is_owner()
+    @commands.hybrid_command()
+    @app_commands.guilds(*Config.GUILD_IDs)
+    @app_commands.describe(module_name="The Python-qualified module name")
+    async def load(self, ctx: commands.Context, module_name: str):
+        """Load a module"""
+        await ctx.defer()
+
+        try:
+            await self.bot.load_extension(module_name)
+            await ctx.send(f"Done loading {module_name}")
+        except commands.ExtensionNotFound:
+            await ctx.send(f"{module_name} was not found in the code")
+        except commands.ExtensionAlreadyLoaded:
+            await ctx.send(f"{module_name} was loaded before")
+
+    @commands.is_owner()
+    @commands.hybrid_command()
+    @app_commands.guilds(*Config.GUILD_IDs)
+    @app_commands.describe(module_name="The Python-qualified module name")
+    async def unload(self, ctx: commands.Context, module_name: str):
+        """Unload a module"""
+        await ctx.defer()
+
+        try:
+            await self.bot.unload_extension(module_name)
+            await ctx.send(f"Done unloading {module_name}")
+        except commands.ExtensionNotFound:
+            await ctx.send(f"{module_name} was not found in the code")
+        except commands.ExtensionNotLoaded:
+            await ctx.send(f"{module_name} was not loaded before")
+
+
+async def setup(bot: commands.AutoShardedBot):
+    await bot.add_cog(OwnerCog(bot))
+    logging.info(f"Cog of {__name__} added!")
+
+
+async def teardown(bot: commands.AutoShardedBot):
+    await bot.remove_cog("OwnerCog")
+    logging.info(f"Cog of {__name__} removed!")

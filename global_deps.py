1face0c06e80bc13991b1963f7664092b1ddc400
Tien Dat Pham <dat20036@gmail.com>
2022-06-14 16:38:12 +0700

    Bump version

--- global_deps.py
+++ global_deps.py
@@ -14,7 +14,7 @@ from database import CRUD
 crud_database = CRUD()
 
 # Stuffs
-__nameless_version__ = "0.0.2-beta"
+__nameless_version__ = "0.1.0-beta"
 needed_permissions = Permissions.none()
 needed_permissions.manage_roles = True
 needed_permissions.manage_channels = True

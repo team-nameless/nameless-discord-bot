9e4f76795e035921770372a84dba1c563ceeebb0
Tien Dat Pham <dat20036@gmail.com>
2022-06-18 14:47:26 +0700

    Use decorator instead of static class method

--- cogs/checks/BaseCheck.py
+++ cogs/checks/BaseCheck.py
@@ -1,12 +1,26 @@
+from typing import Callable
 from discord.ext import commands
 
 __all__ = ["BaseCheck"]
 
 
 class BaseCheck:
+    """Base check class. Containing some useful decorators."""
+
     def __init__(self):
         pass
 
     @staticmethod
-    def is_from_help(ctx: commands.Context):
-        return ctx.invoked_with and ctx.invoked_with == "help"
+    def allow_help_message(fn: Callable[[commands.Context], bool]):
+        """
+        Allow this command to be viewed in the help command.
+        Commonly used for bypassing command-specific checks.
+        Note: this is a decorator.
+        """
+
+        def pred(ctx: commands.Context) -> bool:
+            return fn(ctx) or (
+                ctx.invoked_with is not None and ctx.invoked_with == "help"
+            )
+
+        return pred
--- cogs/checks/MusicCogCheck.py
+++ cogs/checks/MusicCogCheck.py
@@ -11,46 +11,36 @@ class MusicCogCheck(BaseCheck):
         super().__init__()
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def user_and_bot_in_voice(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         return MusicCogCheck.bot_in_voice(ctx) and MusicCogCheck.user_in_voice(ctx)
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def bot_must_play_track_not_stream(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         return MusicCogCheck.bot_must_play_something(
             ctx
         ) and MusicCogCheck.must_not_be_a_stream(ctx)
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def bot_in_voice(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         if not ctx.voice_client:
             raise commands.CheckFailure("I must be in a voice channel.")
 
         return True
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def user_in_voice(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         if not ctx.author.voice:  # pyright: ignore
             raise commands.CheckFailure("You must be in a voice channel.")
 
         return True
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def bot_must_silent(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         vc: wavelink.Player = ctx.voice_client  # pyright: ignore
         if vc and vc.is_playing():
             raise commands.CheckFailure("I must be silenced.")
@@ -58,10 +48,8 @@ class MusicCogCheck(BaseCheck):
         return True
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def bot_must_play_something(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         vc: wavelink.Player = ctx.voice_client  # pyright: ignore
         if vc and not vc.is_playing():
             raise commands.CheckFailure("I need to play something.")
@@ -69,10 +57,8 @@ class MusicCogCheck(BaseCheck):
         return True
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def must_not_be_a_stream(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         vc: wavelink.Player = ctx.voice_client  # pyright: ignore
         if vc and vc.track and vc.track.is_stream():
             raise commands.CheckFailure("I can't use this command on streams.")
@@ -80,10 +66,8 @@ class MusicCogCheck(BaseCheck):
         return True
 
     @staticmethod
+    @BaseCheck.allow_help_message
     def queue_has_element(ctx: commands.Context):
-        if BaseCheck.is_from_help(ctx):
-            return True
-
         vc: wavelink.Player = ctx.voice_client  # pyright: ignore
 
         if vc and vc.queue.is_empty:

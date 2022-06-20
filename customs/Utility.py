0844c7b4e3b644292d4e5d20d42dd738f16e14e6
Tien Dat Pham <dat20036@gmail.com>
2022-06-20 09:54:10 +0700

    Fix get_db_url returning formatted values

--- customs/Utility.py
+++ customs/Utility.py
@@ -17,43 +17,43 @@ class Utility:
 
     @staticmethod
     def get_db_url(
-        config_cls: Optional[Type[config.Config]] = None,
-    ) -> Tuple[str, str, str, str, str, str, str, str]:
+        config_cls: Optional[Type] = None,
+    ) -> Tuple[str, str, str, str, str, str, Optional[int], str]:
         """
         Get the database connection URL based on the config and its components.
         :param config_cls: The class used to retrieve config. Defaults to config.Config.
         :return: Database connection URL
         """
         if hasattr(config_cls, "DATABASE") and (db := config_cls.DATABASE):
-            dialect = db["dialect"]
-            driver = qp(f"+{db['driver']}", safe="+") if db["driver"] else ""
-            username = db["username"] if db["username"] else ""
-            password = qp(f":{db['password']}", safe=":") if db["password"] else ""
-            at = qp("@", safe="@") if username and password else ""
-            host = db["host"]
-            port = qp(f":{db['port']}", safe=":") if db["port"] else ""
-            db_name = db["db_name"]
+            dialect: str = db["dialect"]
+            driver: str = qp(f"+{db['driver']}", safe="+") if db["driver"] else ""
+            username: str = db["username"] if db["username"] else ""
+            password: str = qp(f":{db['password']}", safe=":") if db["password"] else ""
+            at: str = qp("@", safe="@") if username and password else ""
+            host: str = db["host"]
+            port: str = qp(f":{db['port']}", safe=":") if db["port"] else ""
+            db_name: str = db["db_name"]
 
             return (
                 f"{dialect}{driver}://{username}{password}{at}{host}{port}/{db_name}",
                 dialect,
-                driver,
+                db["driver"],
                 username,
-                password,
+                db["password"],
                 host,
-                port,
+                db["port"],
                 db_name,
             )
 
         logging.warning("Using SQLite database as fallback method")
         return (
             "sqlite:///nameless.db",
+            "sqlite",
             "",
             "",
             "",
             "",
-            "",
-            "",
+            None,
             "nameless.db",
         )
 

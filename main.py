8473e91827f9f853136830ba822c580841cb256b
Tien Dat Pham <dat20036@gmail.com>
2022-08-06 17:45:32 +0700

    Fix main.py
    - Fix Nameless() constructor not respecting the new configuration changes
    - Fix edge cases when dealing with "--config_class" flag

--- main.py
+++ main.py
@@ -13,37 +13,38 @@ CONFIG_CLASS_FLAG = "--config_class"
 
 
 def main(args: List[str]):
-    try:
-        cls_arg = [arg for arg in args if arg.startswith(f"{CONFIG_CLASS_FLAG}=")]
+    cls_arg = [arg for arg in args if arg.startswith(f"{CONFIG_CLASS_FLAG}=")]
 
-        if cls_arg:
+    if cls_arg:
+        try:
             cfg = __import__(cls_arg[0][len(f"{CONFIG_CLASS_FLAG}=") :]).NamelessConfig
-        else:
+        except (ValueError, ModuleNotFoundError):
             cfg = NamelessConfig.NamelessConfig
+            logging.warning(
+                "Invalid value for config class flag, NamelessConfig.NamelessConfig will be used"
+            )
+    else:
+        cfg = NamelessConfig.NamelessConfig
 
-        shared_vars.config_cls = cfg
-        prefixes = getattr(cfg, "PREFIXES", [])
-        allow_mention = getattr(cfg, "RECEIVE_MENTION_PREFIX", False)
+    shared_vars.config_cls = cfg
+    prefixes = getattr(cfg, "PREFIXES", [])
+    allow_mention = getattr(cfg, "RECEIVE_MENTION_PREFIX", False)
 
-        intents = discord.Intents.default()
-        intents.message_content = getattr(cfg, "RECEIVE_TEXTS", False)
-        intents.members = getattr(cfg, "RECEIVE_MEMBER_EVENTS", False)
+    intents = discord.Intents.default()
+    intents.message_content = getattr(cfg, "RECEIVE_TEXTS", False)
+    intents.members = getattr(cfg, "RECEIVE_MEMBER_EVENTS", False)
 
-        nameless = Nameless(
-            config_cls=cfg,
-            intents=intents,
-            command_prefix=commands.when_mentioned_or(*prefixes)
-            if allow_mention
-            else prefixes,
-            allow_updates_check=UPDATE_CHECK_FLAG in args,
-            description=getattr(cfg, "BOT_DESCRIPTION", ""),
-        )
+    nameless = Nameless(
+        config_cls=cfg,
+        intents=intents,
+        command_prefix=commands.when_mentioned_or(*prefixes)
+        if allow_mention
+        else prefixes,
+        allow_updates_check=UPDATE_CHECK_FLAG in args,
+        description=getattr(cfg, "META", {}).get("bot_description", ""),
+    )
 
-        nameless.start_bot()
-    except ModuleNotFoundError:
-        logging.error(
-            "You need to provide a NamelessConfig.py file in the same directory with this file!"
-        )
+    nameless.start_bot()
 
 
 if __name__ == "__main__":

7c0ebae55fabbf0df608f8736bb07b4bd67c01fe
Tien Dat Pham <dat20036@gmail.com>
2022-11-22 12:07:45 +0700

    Add stuffs for final release of v1
    - Use sys.exit() in main.py
    - New line at the end of GeneralCog.py
    - Default values for None-able variables in OsuCog.py
    - Add empty code check in OwnerCog.py
    - Guard clause rewrite in crud.py
    - Use "# type: ignore" in models.py
    - Remove leftover self.global_logger in nameless.py
    - Add timeout in shared_vars
    - Bump version to 1.5.2

--- nameless/shared_vars.py
+++ nameless/shared_vars.py
@@ -25,8 +25,12 @@ cogs_regex = re.compile(r"^(?!_.).*Cog.py")
 upstream_version_txt_url = "https://raw.githubusercontent.com/nameless-on-discord/nameless/main/version.txt"
 start_time: datetime = datetime.min
 additional_handlers: List = []
-__nameless_current_version__ = "1.5.0"
-__nameless_upstream_version__ = requests.get(upstream_version_txt_url).text
+__nameless_current_version__ = "1.5.2"
+
+try:
+    __nameless_upstream_version__ = requests.get(upstream_version_txt_url, timeout=10).text
+except requests.exceptions.ConnectTimeout:
+    __nameless_upstream_version__ = ""
 
 # Perms
 needed_permissions = Permissions.none()

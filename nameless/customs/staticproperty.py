c28190bdf2af9dc82786ae09f9c0a656d29eaade
Tien Dat Pham <dat20036@gmail.com>
2023-03-30 11:34:45 +0700

    no wonder why CRUD code is kinda bruh

--- nameless/customs/staticproperty.py
+++ nameless/customs/staticproperty.py
@@ -2,5 +2,5 @@ __all__ = ["staticproperty"]
 
 
 class staticproperty(property):
-    def __get__(self, cls, owner):  # type: ignore
-        return classmethod(self.fget).__get__(None, owner)()
+    def __get__(self, cls, owner):
+        return classmethod(self.fget).__get__(None, owner)()  # pyright: ignore

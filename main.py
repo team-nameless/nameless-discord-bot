2866d45c25ef50d9ac7ef2e729f423302f073c54
Tien Dat Pham <dat20036@gmail.com>
2023-04-05 21:56:32 +0700

    Add try/catch to defer if it has not been responded (mostly for check failures)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import sys
 
 import discord
+from discord import InteractionResponded
 from discord.app_commands import AppCommandError, errors
 from discord.ext import commands
 
@@ -34,14 +35,21 @@ nameless = Nameless(
 # (from d.py official server)
 @nameless.tree.error
 async def on_app_command_error(interaction: discord.Interaction, err: AppCommandError):
-    if not isinstance(err, errors.CommandSignatureMismatch):
-        await interaction.followup.send(f"Something went wrong when executing the command:\n```\n{err}\n```")
+    content = f"Something went wrong when executing the command:\n```\n{err}\n```"
 
-    logging.exception(
-        "[on_command_error] We have gone under a crisis!!!",
-        stack_info=True,
-        exc_info=err,
-    )
+    if not isinstance(err, errors.CommandSignatureMismatch):
+        try:
+            await interaction.response.defer()
+        except InteractionResponded:
+            pass
+
+        await interaction.followup.send(content)
+
+        logging.exception(
+            "[on_command_error] We have gone under a crisis!!!",
+            stack_info=True,
+            exc_info=err,
+        )
 
 
 # If you are encountering the error such as "column not found", this is for you

a756120801e03afd948dbe7d7111b059c4338ef3
Tien Dat Pham <dat20036@gmail.com>
2023-12-04 19:40:36 +0700

    Add "install package" to list of IDE commands.

---
+++ tools/install-deps.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+sep = os.sep
+
+subprocess.run(
+    [
+        "pip",
+        "install",
+        "-r", "requirements_core.txt",
+        "-r", "requirements_dev.txt",
+    ]
+)

44291939676621cc736299e292f1c52454ebe4d6
FoxeiZ <foxeiz2004@gmail.com>
2024-01-07 01:27:54 +0700

    new audio check

--- nameless/cogs/checks/MusicCogCheck.py
+++ nameless/cogs/checks/MusicCogCheck.py
@@ -3,6 +3,7 @@ import wavelink
 from discord.app_commands import CheckFailure
 
 from nameless.cogs.checks import BaseCheck
+from nameless.database import CRUD
 
 __all__ = ["MusicCogCheck"]
 
@@ -10,7 +11,7 @@ __all__ = ["MusicCogCheck"]
 class MusicCogCheck(BaseCheck):
     @staticmethod
     def must_not_be_a_stream(interaction: discord.Interaction):
-        vc: wavelink.Player = interaction.guild.voice_client  # pyright: ignore
+        vc: wavelink.Player = interaction.guild.voice_client  # type: ignore
 
         if vc and vc.current and vc.current.is_stream:
             raise CheckFailure("I can't use this command on streams.")
@@ -41,7 +42,7 @@ class MusicCogCheck(BaseCheck):
 
     @staticmethod
     def queue_has_element(interaction: discord.Interaction):
-        vc: wavelink.Player = interaction.guild.voice_client  # pyright: ignore
+        vc: wavelink.Player = interaction.guild.voice_client  # type: ignore
 
         if MusicCogCheck.user_and_bot_in_voice(interaction) and not bool(vc.queue):
             raise CheckFailure("I need to have something in the queue.")
@@ -50,7 +51,7 @@ class MusicCogCheck(BaseCheck):
 
     @staticmethod
     def bot_is_not_playing_something(interaction: discord.Interaction):
-        vc: wavelink.Player = interaction.guild.voice_client  # pyright: ignore
+        vc: wavelink.Player = interaction.guild.voice_client  # type: ignore
 
         if vc and vc.playing:
             raise CheckFailure("I must not be playing something (or paused while doing it).")
@@ -59,7 +60,7 @@ class MusicCogCheck(BaseCheck):
 
     @staticmethod
     def bot_is_silent(interaction: discord.Interaction):
-        vc: wavelink.Player = interaction.guild.voice_client  # pyright: ignore
+        vc: wavelink.Player = interaction.guild.voice_client  # type: ignore
 
         if vc and not vc.paused:
             raise CheckFailure("I must be silenced.")
@@ -68,9 +69,28 @@ class MusicCogCheck(BaseCheck):
 
     @staticmethod
     def bot_is_playing_something(interaction: discord.Interaction):
-        vc: wavelink.Player = interaction.guild.voice_client  # pyright: ignore
+        vc: wavelink.Player = interaction.guild.voice_client  # type: ignore
 
         if vc and not vc.playing:
             raise CheckFailure("I need to play something.")
+
+        return True
+
+    @staticmethod
+    def has_audio_role(interaction: discord.Interaction):
+        dbg = CRUD.get_or_create_guild_record(interaction.guild)
+        role_id: int
+
+        if dbg and dbg.audio_role_id:
+            role_id = dbg.audio_role_id
+        else:
+            role_id = discord.utils.get(interaction.guild.roles, name="Audio").id  # type: ignore
+            dbg.audio_role_id = role_id
+
+        if not role_id:
+            return True  # guild dont have audio role, just return True I guess
+
+        if interaction.guild and role_id in interaction.user._roles:  # type: ignore
+            raise CheckFailure("You must have an audio role to use this command.")
 
         return True

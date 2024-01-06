c1f37032365c76ff0b6e1c40d7559a3b1d5b5a2b
FoxeiZ <foxeiz2004@gmail.com>
2024-01-07 01:27:36 +0700

    add new row, ignore all warning and remove `type: ignore` comment

--- nameless/database/models.py
+++ nameless/database/models.py
@@ -1,3 +1,4 @@
+# pyright: reportGeneralTypeIssues=false, reportWildcardImportFromLibrary=false
 from datetime import datetime, timedelta
 
 import discord
@@ -16,10 +17,10 @@ class DbUser(Base):
         self.discord_id = _id.id if isinstance(_id, discord.User) else _id
 
     __tablename__ = "Users"
-    discord_id: int = Column(BigInteger, name="Id", primary_key=True)  # type: ignore
-    warn_count: int = Column(SmallInteger, name="WarnCount", default=0)  # type: ignore
-    osu_username: str = Column(Text, name="OsuUsername", default="")  # type: ignore
-    osu_mode: str = Column(Text, name="OsuMode", default="")  # type: ignore
+    discord_id: int = Column(BigInteger, name="Id", primary_key=True)
+    warn_count: int = Column(SmallInteger, name="WarnCount", default=0)
+    osu_username: str = Column(Text, name="OsuUsername", default="")
+    osu_mode: str = Column(Text, name="OsuMode", default="")
 
 
 class DbGuild(Base):
@@ -28,17 +29,18 @@ class DbGuild(Base):
         self.discord_id = _id.id if isinstance(_id, discord.Guild) else _id
 
     __tablename__ = "Guilds"
-    discord_id: int = Column(BigInteger, name="Id", primary_key=True)  # type: ignore
-    is_welcome_enabled: bool = Column(Boolean, name="IsWelcomeEnabled", default=False)  # type: ignore
-    is_goodbye_enabled: bool = Column(Boolean, name="IsGoodbyeEnabled", default=False)  # type: ignore
-    is_bot_greeting_enabled: bool = Column(Boolean, name="IsBotGreetingEnabled", default=True)  # type: ignore
-    is_dm_preferred: bool = Column(Boolean, name="IsDmPreferred", default=False)  # type: ignore
-    is_timeout_preferred: bool = Column(Boolean, name="IsTimeoutPreferred", default=True)  # type: ignore
-    welcome_channel_id: int = Column(BigInteger, name="WelcomeChannelId", default=0)  # type: ignore
-    goodbye_channel_id: int = Column(BigInteger, name="GoodbyeChannelId", default=0)  # type: ignore
-    welcome_message: str = Column(UnicodeText, name="WelcomeMessage", default="")  # type: ignore
-    goodbye_message: str = Column(UnicodeText, name="GoodbyeMessage", default="")  # type: ignore
-    max_warn_count: int = Column(BigInteger, name="MaxWarnCount", default=3)  # type: ignore
-    mute_role_id: int = Column(BigInteger, name="MuteRoleId", default=0)  # type: ignore
-    radio_start_time: datetime = Column(DateTime, name="RadioStartTime", default=datetime.min)  # type: ignore
-    mute_timeout_interval: timedelta = Column(Interval, name="MuteTimeoutInterval", default=timedelta(days=7))  # type: ignore
+    discord_id: int = Column(BigInteger, name="Id", primary_key=True)
+    is_welcome_enabled: bool = Column(Boolean, name="IsWelcomeEnabled", default=False)
+    is_goodbye_enabled: bool = Column(Boolean, name="IsGoodbyeEnabled", default=False)
+    is_bot_greeting_enabled: bool = Column(Boolean, name="IsBotGreetingEnabled", default=True)
+    is_dm_preferred: bool = Column(Boolean, name="IsDmPreferred", default=False)
+    is_timeout_preferred: bool = Column(Boolean, name="IsTimeoutPreferred", default=True)
+    welcome_channel_id: int = Column(BigInteger, name="WelcomeChannelId", default=0)
+    goodbye_channel_id: int = Column(BigInteger, name="GoodbyeChannelId", default=0)
+    welcome_message: str = Column(UnicodeText, name="WelcomeMessage", default="")
+    goodbye_message: str = Column(UnicodeText, name="GoodbyeMessage", default="")
+    max_warn_count: int = Column(BigInteger, name="MaxWarnCount", default=3)
+    mute_role_id: int = Column(BigInteger, name="MuteRoleId", default=0)
+    audio_role_id: int = Column(BigInteger, name="AudioRoleId", default=0)
+    radio_start_time: datetime = Column(DateTime, name="RadioStartTime", default=datetime.min)
+    mute_timeout_interval: timedelta = Column(Interval, name="MuteTimeoutInterval", default=timedelta(days=7))

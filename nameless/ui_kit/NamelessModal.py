527b3229692684e0827e24b21fa7b1a13dbf44c3
Tien Dat Pham <dat20036@gmail.com>
2024-02-12 20:18:53 +0700

    Put `title` kwarg in super().__init__() of NamelessModal class.

--- nameless/ui_kit/NamelessModal.py
+++ nameless/ui_kit/NamelessModal.py
@@ -6,7 +6,7 @@ __all__ = ["NamelessModal"]
 
 class NamelessModal(ui.Modal):
     def __init__(self, *, title: str, initial_text: str | None = None):
-        super().__init__()
+        super().__init__(title=title)
 
         self.title = title
 
@@ -17,8 +17,7 @@ class NamelessModal(ui.Modal):
         label="...",  # this attribute should be edited per-command
         style=discord.TextStyle.paragraph,
         max_length=2000,
-        placeholder="If you need some placeholders, use the 'view placeholders' command",
     )
 
     async def on_submit(self, interaction: discord.Interaction, /) -> None:
-        await interaction.response.send_message("Hang on, your greeter text should be updated soon!")
+        await interaction.response.send_message("Hang on, your response should be acknowledged soon!")

5773976f488a34658e5de9669ab2ec4b27712c1e
Tien Dat Pham <dat20036@gmail.com>
2024-08-07 13:18:54 +0700

    I forgot `tests` exists.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,14 +1,14 @@
-from nameless.commons import Utility
+import nameless.utils as utils
 
 
 class TestUtility:
     def test_url_check_true(self):
-        assert Utility.is_an_url("http://example.com")
-        assert Utility.is_an_url("https://discord.com")
-        assert Utility.is_an_url("osump://696969")
-        assert Utility.is_an_url("//example.com")
+        assert utils.is_an_url("http://example.com")
+        assert utils.is_an_url("https://discord.com")
+        assert utils.is_an_url("osump://696969")
+        assert utils.is_an_url("//example.com")
 
     def test_url_check_false(self):
-        assert not Utility.is_an_url("bao.moe")
-        assert not Utility.is_an_url("discord.com")
-        assert not Utility.is_an_url("m.me")
+        assert not utils.is_an_url("bao.moe")
+        assert not utils.is_an_url("discord.com")
+        assert not utils.is_an_url("m.me")

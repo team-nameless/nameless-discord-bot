5c3071f31ee2a17d76920d1c25c2c869b0a14ba3
Nguyen Ngoc Duy Bao <57582539+FoxeiZ@users.noreply.github.com>
2025-05-26 18:19:53 +0700

    feat: add type check in config. (#54)

--- nameless/config.py
+++ nameless/config.py
@@ -1,11 +1,38 @@
+from datetime import datetime
 from pathlib import Path
 from tomllib import loads
+from typing import TypedDict
 
 __all__ = ["nameless_config"]
+
+
+class NamelessMetadata(TypedDict):
+    version: str
+    description: str
+    support_server: str
+    start_time: datetime
+    is_shutting_down: bool
+
+
+class NamelessCommands(TypedDict):
+    prefixes: list[str]
+
+
+class NamelessBlacklist(TypedDict):
+    users: list[int]
+    guilds: list[int]
+
+
+class NamelessConfig(TypedDict):
+    nameless: NamelessMetadata
+    command: NamelessCommands
+    blacklist: NamelessBlacklist
+
 
 _cfg_path: Path = Path(__file__).parent.parent.absolute() / "nameless.toml"
 
 with open(_cfg_path, encoding="utf-8") as f:
     _content: str = f.read()
 
-nameless_config = loads(_content)
+# Maybe add a type checker here, using the annotation from the TypedDict
+nameless_config: NamelessConfig = NamelessConfig(**loads(_content))  # pyright: ignore[reportAny]
